b691040530c9f7e0d015e94470a0ecdd96c499c9
Possanii <gpossanii@outlook.com>
2024-12-16 16:45:00 -0300

    refactor: Update NestJS API configuration and add Kafka integration
    - Update Node.js version to 20.9.0-slim in Dockerfile
    - Install openssl, procps, and @nestjs/cli in Dockerfile
    - Add container name nest in docker-compose.yaml
    - Change API port from 3001 to 3000 in main.ts
    - Integrate Kafka producer in routes.service.ts
    - Send RouteCreated event to Kafka after creating a new route

--- nestjs-api/src/routes/routes.service.ts
+++ nestjs-api/src/routes/routes.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ICreateRoute } from './dto/create-route.dto';
 import { PrismaService } from '@/database/prisma/prisma.service';
 import { DirectionsService } from '@/maps/directions/directions.service';
+import * as kafkaLib from '@confluentinc/kafka-javascript';
 
 @Injectable()
 export class RoutesService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly directionsService: DirectionsService,
+    @Inject('KAFKA_PRODUCER')
+    private readonly kafkaProducer: kafkaLib.KafkaJS.Producer,
   ) {}
 
   async create(createRoute: ICreateRoute) {
@@ -18,7 +21,7 @@ export class RoutesService {
       });
 
     const legs = routes[0].legs[0];
-    return this.prismaService.route.create({
+    const route = await this.prismaService.route.create({
       data: {
         name: createRoute.name,
         source: {
@@ -47,6 +50,34 @@ export class RoutesService {
         ),
       },
     });
+
+    this.kafkaProducer.send({
+      topic: 'route',
+      messages: [
+        {
+          value: JSON.stringify({
+            event: 'RouteCreated',
+            id: route.id,
+            distance: legs.distance.value,
+            directions: legs.steps.reduce((acc, step) => {
+              acc.push({
+                lat: step.start_location.lat,
+                lng: step.start_location.lng,
+              });
+
+              acc.push({
+                lat: step.end_location.lat,
+                lng: step.end_location.lng,
+              });
+
+              return acc;
+            }, []),
+          }),
+        },
+      ],
+    });
+
+    return route;
   }
 
   findAll() {
